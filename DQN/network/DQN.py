2c7b3b77cf6fca0d62a4c1acfaa6b9199cb7c81c
exaFLOPs <baikdenny@gmail.com>
2025-01-30 16:14:04 +0900

    got stuck at training part

--- DQN/network/DQN.py
+++ DQN/network/DQN.py
@@ -16,3 +16,5 @@ class DQN(nn.Module):
         x = F.relu(self.layer2(x))
         x = self.layer3(x)
         return x
+    
+    
--- DQN/script/run_deterministic.py
+++ DQN/script/run_deterministic.py
@@ -2,6 +2,8 @@ import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np
 from itertools import count
+import math
+import random
 
 import torch
 import torch.nn as nn
@@ -30,6 +32,8 @@ plt.ion()
 # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
 # TAU is the update rate of the target network
 # LR is the learning rate of the ``AdamW`` optimizer
+# Replay buffer capacity
+
 BATCH_SIZE = 128
 GAMMA = 0.99
 EPS_START = 0.9
@@ -37,6 +41,7 @@ EPS_END = 0.05
 EPS_DECAY = 1000
 TAU = 0.005
 LR = 1e-4
+capa = 10000 
 
 # Get number of actions from gym action space
 n_actions = env.action_space.n
@@ -50,12 +55,41 @@ target_net = DQN(n_observations, n_actions).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 
 optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
-memory = ReplayBuffer(10000)
+memory = ReplayBuffer(capacity= capa)
 
 
 steps_done = 0
 
-def select_action(state):
+# As mentioned by the name of this file, we will be implementing a deterministic policy.
+def select_action(state): 
     global steps_done
     sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold: # Select the action that has the maximum expected return
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else: # Select a random action - exploration (epsilon-greedy)
+        return torch.tensor([[random.randrange(n_actions)], device=device, dtype=torch.long])
     
+epsilon_durations = []
+
+def plot_durations(show_result = False):
+    plt.figure(1)
+    duration_t = torch.tensor(epsilon_durations, dtype=torch.float)
+    
+    if show_result:
+        plt.title("Result")
+    else:
+        plt.clf()
+        plt.title("Training...")
+    plt.xlabel("Episode")
+    plt.ylabel("Duration")
+    plt.plot(duration_t.numpy())
+    
+    if len(duration_t) >= 100:
+        means = duration_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+        
+    plt.pause(0.001) # pause a bit so that plots are updated
