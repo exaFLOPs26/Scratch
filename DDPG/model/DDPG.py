76ae9fb275d4a78f71f96af9eed7e35f202e7a65
exaFLOPs <baikdenny@gmail.com>
2025-02-03 21:35:11 +0900

    convert to simple network

--- DDPG/model/DDPG.py
+++ DDPG/model/DDPG.py
@@ -5,15 +5,13 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     def __init__(self, state_size, action_size):
         super(QNetwork, self).__init__()
-        self.fc1 = nn.Linear(state_size + action_size, 400)
-        self.bn1 = nn.BatchNorm1d(400)
-        self.fc2 = nn.Linear(400, 300)
-        self.bn2 = nn.BatchNorm1d(300)
-        self.fc3 = nn.Linear(300, 1)
+        self.fc1 = nn.Linear(state_size + action_size, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 1)  # Output a single Q-value
 
     def forward(self, state, action):
-        x = torch.cat([state, action], dim=1)
-        x = F.relu(self.bn1(self.fc1(x)))
-        x = F.relu(self.bn2(self.fc2(x)))
+        x = torch.cat([state, action], dim=1)  # Concatenate state and action
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
