057c0254c26092060a343229a41d6db0e6ae3ad9
exaFLOPs <baikdenny@gmail.com>
2025-02-03 21:14:49 +0900

    batch normalization

--- DDPG/policy/action_net.py
+++ DDPG/policy/action_net.py
@@ -1,8 +1,6 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 
 def noise(n_act, noise_factor):
     return torch.randn(n_act) * noise_factor
@@ -10,12 +8,14 @@ def noise(n_act, noise_factor):
 class actorNetwork(nn.Module):
     def __init__(self, state_size, action_size):
         super(actorNetwork, self).__init__()
-        self.fc1 = nn.Linear(state_size, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, action_size)
+        self.fc1 = nn.Linear(state_size, 400)
+        self.bn1 = nn.BatchNorm1d(400)
+        self.fc2 = nn.Linear(400, 300)
+        self.bn2 = nn.BatchNorm1d(300)
+        self.fc3 = nn.Linear(300, action_size)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = torch.tanh(self.fc3(x))  # Use tanh to ensure actions are within the range [-1, 1]
         return x
